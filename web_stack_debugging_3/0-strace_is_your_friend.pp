b798369ea566791b82c9af1446c68d77909aed3f
nkubana0 <i.shema@alustudent.com>
2023-12-16 14:47:03 -0800

    strace

---
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# for debugging apache2 to fix its 500 error message
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
